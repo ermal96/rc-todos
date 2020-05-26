4293df5c65599b9ca3def2f300a8adaa69ccd2d3
Ermal Vrapi <ermal.vrapi@crispybacon.al>
2020-05-26 18:13:32 +0200

    updated menu with navitem

--- src/containers/todos/FilterMenu.js
+++ src/containers/todos/FilterMenu.js
@@ -1,44 +1,18 @@
 import React from 'react'
 import {Menu} from 'antd';
-import {Link} from "react-router-dom";
 import * as ROUTES from '../../constants/routes'
 import {DatabaseOutlined, CarryOutOutlined, PlayCircleOutlined} from '@ant-design/icons';
-import { useLocation } from "react-router-dom";
+import NavItem from '../nav/NavItem';
+
 
 const FilterMenu = () => {
-
-    let location = useLocation();
-
-    let defPath = 'allTodos';
     
-    if(location.pathname === ROUTES.HOME){
-        defPath = 'allTodos'
-    }else if (location.pathname === ROUTES.ACTIVE){
-        defPath = 'active'
-    }else if (location.pathname === ROUTES.COMPLETED){
-        defPath = 'completed'
-    }else {
-        defPath = 'allTodos'
-    }
-
     return (
-        <Menu selectedKeys={defPath}
-        mode="horizontal">
+        <Menu mode="horizontal">
         
-        <Menu.Item key="allTodos"
-            icon={<DatabaseOutlined />}>
-                <Link to={ROUTES.HOME}>All</Link>
-             </Menu.Item>
-
-         <Menu.Item key="active"
-            icon={<PlayCircleOutlined />}>
-            <Link to={ROUTES.ACTIVE}>Active</Link>
-             </Menu.Item>
-
-        <Menu.Item key="completed"
-            icon={<CarryOutOutlined/>}>
-            <Link to={ROUTES.COMPLETED}>Completed</Link>
-        </Menu.Item>
+        <NavItem isPrivate link={ROUTES.HOME} icon={<DatabaseOutlined />} name="All"/>
+        <NavItem isPrivate link={ROUTES.ACTIVE} icon={<PlayCircleOutlined />} name="Active"/>
+        <NavItem isPrivate link={ROUTES.COMPLETED} icon={<CarryOutOutlined />} name="Completed"/>
 
     </Menu>
     )
