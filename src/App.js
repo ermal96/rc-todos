f0888595f7368bed094bff9df96418476300f6b1
Ermal Vrapi <ermal.vrapi@crispybacon.al>
2020-05-26 10:45:27 +0200

    added redirect to AllTodos component if url doesn't match

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Layout from './containers/Layout';
 import Login from './containers/auth/Login';
 import Register from './containers/auth/Register';
 import Logout from './containers/auth/Logout'
-import {BrowserRouter as Router, Switch} from "react-router-dom";
+import {BrowserRouter as Router,  Redirect} from "react-router-dom";
 import PrivateRoute  from './components/routes/PrivateRoute'
 import PublicRoute  from './components/routes/PublicRoute'
 import * as ROUTES from './constants/routes'
@@ -25,32 +25,19 @@ const App = ({initFirebase, isFirebaseloaded}) => {
         return (
             <Router>
                 <Layout>
-                    <Switch>
-                        <PrivateRoute exact path={ROUTES.TODOS}>
-                            <AllTodos/>
-                        </PrivateRoute>
+                        <PrivateRoute exact path={ROUTES.TODOS} component={AllTodos} />
 
-                        <PrivateRoute  exact path={ROUTES.TODOS + ROUTES.ACTIVE}>
-                            <ActiveTodos/>
-                        </PrivateRoute>
+                        <Redirect path={ROUTES.HOME} to={AllTodos} />
 
-                        <PrivateRoute  exact path={ROUTES.TODOS + ROUTES.COMPLETED}>
-                            <CompletedTodos/>
-                        </PrivateRoute>
-    
-                        <PublicRoute exact path={ROUTES.LOGIN}>
-                            <Login/>
-                        </PublicRoute>
-    
-                        <PublicRoute exact path={ROUTES.REGISTER}>
-                            <Register/>
-                        </PublicRoute>
-    
-                        <PrivateRoute exact path={ROUTES.LOGOUT}>
-                            <Logout/>
-                        </PrivateRoute>
-
-                    </Switch>
+                        <PrivateRoute  exact path={ROUTES.TODOS + ROUTES.ACTIVE} component={ActiveTodos} />
+                           
+                        <PrivateRoute  exact path={ROUTES.TODOS + ROUTES.COMPLETED} component={CompletedTodos} />
+                           
+                        <PublicRoute   exact path={ROUTES.LOGIN} component={Login} />
+                            
+                        <PublicRoute   exact path={ROUTES.REGISTER} component={Register} />
+                           
+                        <PrivateRoute  exact path={ROUTES.LOGOUT} component={Logout} />  
                 </Layout>
             </Router>
         );
