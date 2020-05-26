6427d7a07cb38400ca7738a70c2c900e40151874
Ermal Vrapi <ermal.vrapi@crispybacon.al>
2020-05-26 18:14:06 +0200

    added user update

--- src/store/actions/authActions.js
+++ src/store/actions/authActions.js
@@ -5,13 +5,22 @@ import {SuccessMsg, ErrorMsg} from '../../utils/helpers/msg'
 export const register = (data) => async dispatch => {
     Firebase.auth().createUserWithEmailAndPassword(data.email, data.password).then((res) => {
 
-        Firestore.collection("users").doc(res.user.uid).set({
-            firstName: data.firstName,
-            lastName: data.lastName
-        }).then(() => {
-            dispatch({type: types.REGISTER_SUCCESS})
-            SuccessMsg('Successfully Registered')
-        })
+        const  user = Firebase.auth().currentUser;
+
+        user.updateProfile({
+            displayName: data.displayName,
+          }).then(() => {
+
+            user.sendEmailVerification().then(() => {
+                SuccessMsg('Please verify your email')
+            }).catch(function(err) {
+                ErrorMsg(err.message)
+            });
+            
+          }).catch(function(err) {
+            ErrorMsg(err.message)
+          });
+
         
     }).catch(err => {
         ErrorMsg(err.message)
@@ -21,13 +30,9 @@ export const register = (data) => async dispatch => {
 
 export const login = (data) => async dispatch => {
     Firebase.auth().signInWithEmailAndPassword(data.email, data.password).then((res) => {  
-        const userId = res.user.uid;
+        
+        SuccessMsg(`Welcome ${res.user.displayName}`)
 
-        Firestore.collection('users').doc(userId).get().then(res => {
-            const name  = res.data().firstName
-            SuccessMsg(`Welcome ${name}`)
-        })
-       
     }).catch(err => {
         ErrorMsg(err.message)
     })
@@ -36,26 +41,17 @@ export const login = (data) => async dispatch => {
 
 export const logout = () => async (dispatch, getState) => {
 
-    const userId  = getState().firebase.user.uid
+    const name  = getState().firebase.user.displayName
 
-    Firestore.collection('users').doc(userId).get().then(res => {
-
-        const name  = res.data().firstName
-
-        Firebase.auth().signOut().then(() => {
-            SuccessMsg('Bye ' + name + ' see you soon ' +  String.fromCharCode(0xD83D, 0xDE04))
-            dispatch({
-                type: types.LOGOUT,
-                payload: ''
-            })
-        }).catch(err => {
-            ErrorMsg(err.message)
+    Firebase.auth().signOut().then(() => {
+        SuccessMsg('Bye ' + name + ' see you soon ' +  String.fromCharCode(0xD83D, 0xDE04))
+        dispatch({
+            type: types.LOGOUT,
+            payload: ''
         })
-
-
+    }).catch(err => {
+        ErrorMsg(err.message)
     })
-
-
 }
 
 export const initFirebase = () => async dispatch => {
@@ -75,4 +71,69 @@ export const initFirebase = () => async dispatch => {
 
 
       });
+}
+
+
+export const updateUser = (data) => async (dispatch) => {
+
+    const user = Firebase.auth().currentUser;
+
+
+    if(data.displayName || data.password || data.email ){
+
+        // update email if user provide a email && send email confirmation to user
+    if(data.email){
+        user.updateEmail(data.email).then(() =>  {
+            
+            user.sendEmailVerification().then(() => {
+                dispatch({
+                    type: types.USER_EMAIL_UPDATED,
+                    payload: data.email
+                })
+              }).catch(function(err) {
+                ErrorMsg(err.message)
+              });
+
+          }).catch((err) => {
+            ErrorMsg(err.message)
+          });
+    }
+
+
+    //update password if user provide a password
+    if(data.password){
+        user.updatePassword(data.password).then(() => {
+            dispatch({
+                type: types.USER_PASSWORD_UPDATED,
+            })
+          }).catch(function(err) {
+            ErrorMsg(err.message)
+          });
+    }
+
+
+    // update name  if user provode it
+    if(data.displayName){
+        user.updateProfile({
+            displayName: data.displayName,
+          }).then(() => {
+            dispatch({
+                type: types.USER_NAME_UPDATED,
+                payload: data.displayName
+            })
+          }).catch(function(err) {
+            ErrorMsg(err.message)
+          });
+    }
+
+
+
+        SuccessMsg('Successfully updated')
+
+    }
+
+
+
+
+
 }
